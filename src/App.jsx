921ab8f4ee32477b8894e41228ca58500cdf08d7
Abe Sanchez <abe.sanchez.work@gmail.com>
2022-10-17 13:22:53 +0200

    config eslint console

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getProductList, selectProducts, selectError } from "./store/products";
+import { useDispatch } from "react-redux";
+import { getProductList } from "./store/products";
 
 function App() {
   const dispatch = useDispatch();
-  const products = useSelector(selectProducts);
-  console.log(products);
 
   useEffect(() => {
     dispatch(getProductList());
