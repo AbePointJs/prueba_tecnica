142b04cc5f8ecb5efe901fd4a7bcc46095450754
AbePointJs <97465953+AbePointJs@users.noreply.github.com>
2022-10-23 18:05:24 +0200

    fix icon err

--- src/components/__tests__/Icon.test.jsx
+++ src/components/__tests__/Icon.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 import Icon from "../icon/Icon";
+import "@testing-library/jest-dom/extend-expect";
 
 function delay(time) {
   return new Promise((res) => {
